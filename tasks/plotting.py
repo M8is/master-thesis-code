e461b13c30dbc5460b38055231d9a869a5e78489
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2021-05-31 19:22:35 +0200

    Add plot titles

--- tasks/plotting.py
+++ tasks/plotting.py
@@ -9,10 +9,13 @@ def plot_losses(plot_dir, losses_per_task):
             makedirs(plot_dir)
 
         print(f"Plotting losses for task '{task}' in '{plot_dir}' ...")
+        dataset = '<unnamed dataset>'
         for config, losses in configs_and_losses.values():
             train_losses, _ = zip(*losses)
             __plot(np.stack(train_losses), **config)
+            dataset = config['dataset']
         plt.legend()
+        plt.title(f"{dataset} train loss")
         plt.savefig(path.join(plot_dir, 'train.png'))
         plt.clf()
 
@@ -20,6 +23,7 @@ def plot_losses(plot_dir, losses_per_task):
             _, test_losses = zip(*losses)
             __plot(np.stack(test_losses), **config)
         plt.legend()
+        plt.title(f"{dataset} test loss")
         plt.savefig(path.join(plot_dir, 'test.png'))
         plt.clf()
 
@@ -29,4 +33,4 @@ def __plot(losses, **kwargs):
     mean = losses.mean(axis=0)
     std = losses.std(axis=0)
     plt.plot(mean, label=kwargs.get('plot_label', kwargs['mc_estimator']), linewidth=.5)
-    plt.fill_between(range(len(mean)), mean - 2 * std, mean + 2 * std, alpha=.3)
+    plt.fill_between(range(len(mean)), mean - std, mean + std, alpha=.3)
