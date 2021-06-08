cdbbb8662b66546fae9e29ee37e7f3074bae3c79
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2021-06-08 11:02:00 +0200

    Remove obsolete flag

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ from utils.seeds import fix_random_seed
 
 
 def main(args):
-    if args.plot and args.clean:
-        raise ValueError("Clean and plot is not allowed to avoid accidentally deleting results.")
-
     config_path = args.CONFIG
     with open(config_path, 'r') as f:
         meta_config = yaml.safe_load(f)
@@ -50,9 +47,9 @@ def main(args):
             if args.clean:
                 clean(**config)
 
-            task = config.get('task', None)
+            task = config.get('task')
             train_loss = LossHolder(results_dir, train=True)
-            estimator_stds = TensorHolder(results_dir, 'estimator_stds')
+            estimator_stds = TensorHolder(results_dir, 'estimator_stds') if config.get('compute_variance') else None
             if not train_loss.is_empty():
                 print(f"Skipping training; Loading existing results from '{results_dir}'...")
                 test_loss = LossHolder(results_dir, train=False)
@@ -77,7 +74,7 @@ def main(args):
             else:
                 losses_per_task[task][config_subpath][1].append((train_loss.numpy(), test_loss.numpy()))
 
-            if not estimator_stds.is_empty():
+            if estimator_stds is not None and not estimator_stds.is_empty():
                 if task not in stds_per_task:
                     stds_per_task[task] = dict()
 
@@ -86,9 +83,8 @@ def main(args):
                 else:
                     stds_per_task[task][config_subpath][1].append(estimator_stds.numpy())
 
-    if args.plot:
-        plot_losses(results_base_dir, losses_per_task)
-        plot_estimator_variances(results_base_dir, stds_per_task)
+    plot_losses(results_base_dir, losses_per_task)
+    plot_estimator_variances(results_base_dir, stds_per_task)
 
 
 if __name__ == '__main__':
@@ -96,5 +92,4 @@ if __name__ == '__main__':
     parser.add_argument('CONFIG', help='path to config file')
     parser.add_argument('--clean', action='store_true',
                         help='WARNING: deletes all result directories and starts a clean run')
-    parser.add_argument('--plot', action='store_true', help='Run plotting')
     main(parser.parse_args())
