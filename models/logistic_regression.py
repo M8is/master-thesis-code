c6b3011afe575caa4120d99372260bb68db32591
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2021-06-08 11:01:37 +0200

    Fix logreg

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -2,19 +2,16 @@ import torch
 
 
 class LinearLogisticRegression(torch.nn.Module):
-    def __init__(self, latent_dims, probabilistic):
+    def __init__(self, params_size, probabilistic):
         super().__init__()
-        self.params = torch.nn.Parameter(torch.randn((sum(latent_dims),)))
+        self.raw_params = torch.nn.Parameter(torch.randn((1, params_size)))
         self.probabilistic = probabilistic
 
     def forward(self, x):
-        raw_params = self.params.unsqueeze(0).repeat_interleave(x.size(0), dim=0)
-        samples = self.probabilistic(raw_params) if self.training else self.probabilistic.distribution.sample(raw_params)
+        raw_params = self.raw_params.repeat_interleave(x.size(0), dim=0)
+        samples = self.probabilistic(raw_params)
         return raw_params, self.predict(samples, x)
 
-    def predict(self, samples, x):
-        return self.__logistic((x * samples).sum(dim=-1))
-
     @staticmethod
-    def __logistic(z):
-        return 1 / (1 + torch.exp(-z))
+    def predict(samples, x):
+        return torch.sigmoid((x * samples).sum(dim=-1))
--- tasks/train_log_reg.py
+++ tasks/train_log_reg.py
@@ -1,3 +1,4 @@
+from math import prod
 from os import path
 
 import torch
@@ -10,13 +11,14 @@ from utils.eval_util import eval_mode
 from utils.tensor_holders import LossHolder
 
 
-def train_log_reg(results_dir, dataset, device, param_dims, epochs, sample_size, learning_rate, mc_estimator,
-                  distribution, batch_size, **kwargs):
+def train_log_reg(results_dir, dataset, device, epochs, sample_size, learning_rate, mc_estimator, distribution,
+                  batch_size, **kwargs):
     data_holder = DataHolder.get(dataset, batch_size)
+    n_features = prod(data_holder.dims)
 
     # Create model
-    estimator = get_estimator(mc_estimator, distribution, sample_size, device, param_dims, **kwargs)
-    model = models.logistic_regression.LinearLogisticRegression(param_dims, estimator).to(device)
+    estimator = get_estimator(mc_estimator, distribution, sample_size, device, n_features, **kwargs)
+    model = models.logistic_regression.LinearLogisticRegression(sum(estimator.param_dims), estimator).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     print(f'Training with {estimator}.')
@@ -28,9 +30,9 @@ def train_log_reg(results_dir, dataset, device, param_dims, epochs, sample_size,
         test_losses.add(test_loss.mean())
         train_losses.save()
         test_losses.save()
-        print(f"Epoch: {epoch}/{epochs}, Train loss: {train_losses.numpy()[-1]:.2f}, "
-              f"Test loss: {test_losses.numpy()[-1]:.2f}",
-              flush=True)
+        if epoch % 5 == 0:
+            print(f"Epoch: {epoch}/{epochs}, Train loss: {train_losses.numpy()[-1]:.2f}, "
+                  f"Test loss: {test_losses.numpy()[-1]:.2f}")
     torch.save(model, path.join(results_dir, f'{mc_estimator}_{epochs}.pt'))
 
     return train_losses, test_losses
@@ -41,16 +43,15 @@ def __train_epoch(model, data_holder, device, optimizer):
     test_losses = []
     model.train()
     for batch_id, (x_batch, y_batch) in enumerate(data_holder.train):
-        raw_params, y_preds = model(x_batch.to(device))
-        loss = __bce_loss(y_batch.to(device), y_preds).mean()
+        x_batch = x_batch.flatten(start_dim=1).to(device)
+        y_batch = y_batch.to(device).double()
+        raw_params, y_preds = model(x_batch)
+        loss = __bce_loss(y_batch, y_preds).mean()
         kld = model.probabilistic.distribution.kl(raw_params).mean()
         optimizer.zero_grad()
         kld.backward(retain_graph=True)
         model.probabilistic.backward(raw_params, lambda samples: __bce_loss(y_batch, model.predict(samples, x_batch)))
-        loss.backward()
         optimizer.step()
-        if batch_id % 100 == 0:
-            print(f"\r| ELBO: {-(loss + kld):.2f} | BCE loss: {loss:.1f} | KL Divergence: {kld:.1f} |")
         train_losses.append(loss + kld)
     test_losses.append(__test_epoch(model, data_holder, device))
     return torch.stack(train_losses), torch.stack(test_losses)
@@ -61,8 +62,9 @@ def __test_epoch(model, data_holder, device):
         test_losses = []
         for x_batch, y_batch in data_holder.test:
             x_batch = x_batch.to(device)
+            y_batch = y_batch.to(device).double()
             raw_params, y_preds = model(x_batch)
-            loss = __bce_loss(y_batch.to(device), y_preds).mean()
+            loss = __bce_loss(y_batch, y_preds).mean()
             kld = model.probabilistic.distribution.kl(raw_params).mean()
             test_losses.append(loss + kld)
         return torch.tensor(test_losses).mean()
@@ -71,4 +73,4 @@ def __test_epoch(model, data_holder, device):
 def __bce_loss(y, y_pred):
     # Use no reduction to get separate losses for each image
     binary_cross_entropy = torch.nn.BCELoss(reduction='none')
-    return binary_cross_entropy(y_pred, y.expand_as(y_pred)).sum(dim=-1)
+    return binary_cross_entropy(y_pred, y.expand_as(y_pred))
