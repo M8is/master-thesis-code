c83ec3bde5c4ccc064a55175d94d810bd7b41931
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2021-07-19 12:25:57 +0200

    Categorical experiments

--- distributions/categorical.py
+++ distributions/categorical.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Any, Callable
 
 import torch
-import torch.nn.functional as F
+from torch.nn.functional import gumbel_softmax
 
 from .distribution_base import Distribution
 
@@ -13,14 +13,21 @@ class CategoricalDomain(Enum):
 
 
 class Categorical(Distribution):
-    def __init__(self, *args, domain: int = 1, **kwargs):
+    def __init__(self, *args, domain: int = 1, gumbel_temperature: float = 1., **kwargs):
         super().__init__(*args, **kwargs)
         self.warned = False
+        self.gumbel_temperature = gumbel_temperature
 
         if CategoricalDomain(domain) == CategoricalDomain.ZERO_ONE:
             self.sample = self.with_normalized_outputs(self.sample)
             self.rsample = self.with_normalized_outputs(self.rsample)
-            self.mvsample = self.with_normalized_outputs(self.mvsample)
+            self.domain = torch.linspace(0, 1, self.num_categories)
+        elif CategoricalDomain(domain) == CategoricalDomain.INTEGER:
+            self.domain = torch.arange(self.num_categories)
+
+    @property
+    def num_categories(self):
+        return self.params.shape[-1]
 
     def _as_params(self, raw_params):
         return torch.softmax(raw_params.squeeze(), -1).unsqueeze(0)
@@ -29,27 +36,28 @@ class Categorical(Distribution):
         return torch.distributions.Categorical(self.params).sample(sample_shape)
 
     def rsample(self, sample_shape: torch.Size = torch.Size([])):
-        return self.__as_index(F.gumbel_softmax(torch.log(self.params).expand((*sample_shape, *self.params.shape)),
-                                                hard=True))
+        log_params = torch.log(self.params).expand((*sample_shape, *self.params.shape))
+        return self.__as_index(gumbel_softmax(log_params, hard=True, tau=self.gumbel_temperature))
 
     def mvsample(self, size):
         if size > 1 and not self.warned:
-            print("[WARNING] Categorical MVD ignores sample sizes greater than one.")
+            print("[WARNING] Categorical mvsample ignores sample sizes greater than one.")
             self.warned = True
-        return torch.arange(self.params.size()[-1], device=self.params.device).unsqueeze(0)
+        return self.domain.clone().to(self.params.device).unsqueeze(0)
 
     def mvd_backward(self, losses, retain_graph):
         with torch.no_grad():
-            repeated = losses.repeat_interleave(losses.size()[-1], dim=-2)
             diag = losses.diag_embed()
-            grad = (2 * diag - repeated).sum(dim=-1)
+            repeated = losses.repeat_interleave(self.num_categories, dim=-2)
+            repeated.diagonal().zero_()
+            repeated /= self.num_categories - 1
+            grad = (diag - repeated).sum(dim=-1)
         params = self.params.mean(dim=0, keepdim=True)
         assert grad.shape == params.shape, f"Grad shape {grad.shape} != params shape {params.shape}"
         params.backward(grad, retain_graph=retain_graph)
 
     def pdf(self):
-        x = torch.arange(self.params.size(-1))
-        return x, self.params.detach().squeeze(0)
+        return self.domain.clone(), self.params.detach().squeeze(0)
 
     def kl(self):
         p = torch.distributions.Categorical(self.params)
@@ -60,7 +68,7 @@ class Categorical(Distribution):
         return torch.distributions.Categorical(self.params).log_prob(value)
 
     def with_normalized_outputs(self, f: Callable[[Any], torch.Tensor]) -> Callable[[Any], torch.Tensor]:
-        return lambda *args, **kwargs: f(*args, **kwargs) / self.params.size()[-1]
+        return lambda *args, **kwargs: f(*args, **kwargs) / self.num_categories
 
     @staticmethod
     def __as_index(one_hot_encoding: torch.Tensor) -> torch.Tensor:
