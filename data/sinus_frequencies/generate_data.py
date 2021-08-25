79081496595bfe40c37f6bbd8610da559ddd0deb
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2021-08-25 17:11:14 +0200

    Fix sinus data names

--- data/sinus_frequencies/generate_data.py
+++ data/sinus_frequencies/generate_data.py
@@ -3,18 +3,15 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 
-np.random.seed(66)
-
-
 def sinus_funcion(f, A, t):
-    return A * np.sin(2*np.pi*f*(t + 0.005 * np.random.randn(len(t)))) + 0.01*A*np.random.randn(len(t))
+    return A * np.cos(2*np.pi*f*t) + 0.1*A*np.random.randn(len(t))
 
 
 Amp = 1.
-freqs = [0.05, 0.1, 0.5, 1.0, 2.0]
-L = 400
-N = 25000
-times = np.linspace(0, 6, L, dtype=np.float32)
+freqs = [0.01, 0.05, 0.1, 0.5]
+L = 200
+N = 5000
+times = np.linspace(0, 10, L, dtype=np.float32)
 X = np.zeros((N, L), dtype=np.float32)
 Y = np.zeros((N, 1), dtype=np.float32)
 for n in range(N):
@@ -30,7 +27,8 @@ np.savetxt('./sinus_frequencies_Y_TRAIN', Y_train, delimiter=',')
 np.savetxt('./sinus_frequencies_X_TEST', X_test, delimiter=',')
 np.savetxt('./sinus_frequencies_Y_TEST', Y_test, delimiter=',')
 
-npoints = 128
-plt.plot(X[:npoints].T)
-print(Y[:npoints])
+# exit()
+plt.plot(X[:5].T)
+print(Y[:5])
 plt.show()
+
